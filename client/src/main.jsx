46d32fbcb5577bc473d80c35d51d9f3f46f64711
Developer Barry <barryallen2558@gmail.com>
2024-11-18 19:54:48 +0600

    router error fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,11 +14,26 @@ const router = createBrowserRouter(
       path: '/',
       element: <Root />
     }
-  ]
+  ],
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+      v7_fetcherPersist: true,
+      v7_normalizeFormMethod: true,
+      v7_partialHydration: true,
+      v7_skipActionErrorRevalidation: true,
+    },
+  }
 )
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider
+      router={router}
+      future={{
+        v7_startTransition: true,
+      }}
+    />
   </StrictMode>,
 )
